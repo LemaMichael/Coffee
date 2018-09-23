4678dd58fa6c1ba81214dcdeb39f1f0ab2031c3f
Michael Lema <mel526@nyu.edu>
2018-09-23 18:49:17 -0400

    Added User Defaults Extension

---
+++ Coffee/Helpers/UserDefaultsExtension.swift
@@ -0,0 +1,31 @@
+//
+//  UserDefaultsExtension.swift
+//  Coffee
+//
+//  Created by Michael Lema on 9/23/18.
+//  Copyright © 2018 Michael Lema. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UserDefaults {
+    enum UserDefaultKeys: String {
+        case newsRead
+    }
+    
+    func setNewsAsRead(value: Int) {
+        var readArray = getNewsRead()
+        if !readArray.contains(value) {
+            readArray.append(value)
+        }
+        set(value, forKey: UserDefaultKeys.newsRead.rawValue)
+        synchronize()
+    }
+    func getNewsRead() -> [Int] {
+        guard let readArray = array(forKey: UserDefaultKeys.newsRead.rawValue) as? [Int] else { return [] }
+        return readArray
+    }
+    
+    
+}
