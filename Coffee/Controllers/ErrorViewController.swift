4bd2d112d79ff56030771199e58617b65d9ad44a
Michael Lema <mel526@nyu.edu>
2018-09-27 03:59:58 -0400

    Fixed Error View controller

--- Coffee/Controllers/ErrorViewController.swift
+++ Coffee/Controllers/ErrorViewController.swift
@@ -11,20 +11,42 @@ import UIKit
 
 class ErrorViewController: UIViewController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var checkTimer: Timer!
+    
+    lazy var imageView: UIImageView = {
         let imageView = UIImageView(image: UIImage(named: "Error"))
         imageView.contentMode = .scaleAspectFit
-        view.backgroundColor = UIColor(red:0.93, green:0.93, blue:0.88, alpha:1.00)
+        return imageView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(red:0.93, green:0.93, blue:0.88, alpha:1.00)
         self.view.addSubview(imageView)
+        
         imageView.anchor(top: view.topAnchor, bottom: view.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 0, paddingBottom: 0, paddingLeft: 0, paddingRight: 0, width: 0, height: 0)
+        
+        checkTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(checkWifi), userInfo: nil, repeats: true)
+    }
+    
+    @objc func checkWifi() {
+        if Reachability.isConnectedToNetwork() {
+            self.navigationController?.pushViewController(ViewController(), animated: false)
+        }
     }
     
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        checkTimer.invalidate()
+    }
+    
 }
